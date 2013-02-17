11e4f6cd92a2c5d4b01c7d334002074b19b09303
John O'Shea <john.m.oshea@gmail.com>
2013-02-17 11:06:24 +0000

    Update install.sh to know a bit more about submodules

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+DOTFILES='~/Code/dotfiles'
+ZPREZTO='~/Code/zprezto'
+
 if [ x"$1" = x"new" ]; then
     REMOVE_EXISTING=1
 fi
@@ -8,10 +11,22 @@ fi
 set -e
 set -u
 
-echo "Checking out repos"
-test -d ~/.oh-my-zsh || git clone https://github.com/johnoshea/oh-my-zsh.git ~/Code/oh-my-zsh
+# Basic directory structure
+test -d ~/Code || mkdir ~/Code
 
-if [ ${REMOVE_EXISTING} ]; then
+update_repos () {
+    echo "Updating repos"
+    cd ${ZPREZTO} && git pull
+    cd ${DOTFILES} && git pull
+}
+
+checkout_repos () {
+    echo "Checking out repos"
+    rm -rf ${ZPREZTO} && clone https://github.com/johnoshea/zprezto.git ${ZPREZTO}
+    rm -rf ${DOTFILES} && clone https://github.com/johnoshea/dotfiles ${DOTFILES}
+}
+
+remove_dotfiles () {
     echo "Removing existing home dotfiles"
     rm -f ~/.ackrc
     rm -f ~/.css
@@ -20,46 +35,69 @@ if [ ${REMOVE_EXISTING} ]; then
     rm -f ~/.inputrc
     rm -f ~/.rvmrc
     rm -f ~/.ssh/config
+    rm -f ~/.terminfo
     rm -f ~/.tmux.conf
+    rm -f ~/.terminfo
     rm -f ~/.vim
     rm -f ~/.vim_colors
     rm -f ~/.vimrc
+    rm -f ~/.zlogin
+    rm -f ~/.zlogout
+    rm -f ~/.zprezto
+    rm -f ~/.zpreztorc
+    rm -f ~/.zprofile
+    rm -f ~/.zshenv
     rm -f ~/.zshrc
-    rm -f ~/.oh-my-zsh
+}
+
+
+create_symlinks () {
+    echo "Creating dotfile symlinks"
+    ln -s ${DOTFILES}/ackrc ~/.ackrc
+    ln -s ${DOTFILES}/dotcss ~/.css
+    ln -s ${DOTFILES}/dotjs ~/.js
+    ln -s ${DOTFILES}/gvimrc ~/.gvimrc
+    ln -s ${DOTFILES}/inputrc ~/.inputrc
+    ln -s ${DOTFILES}/rvmrc ~/.rvmrc
+    ln -s ${DOTFILES}/ssh_config ~/.ssh/config
+    ln -s ${DOTFILES}/tmux.conf ~/.tmux.conf
+    ln -s ${DOTFILES}/terminfo ~/.terminfo
+    ln -s ${DOTFILES}/vim ~/.vim
+    ln -s ${DOTFILES}/vim_colors ~/.vim_colors
+    ln -s ${DOTFILES}/vimrc ~/.vimrc
+
+    ln -s ${ZPREZTO}/zlogin ~/.zlogin
+    ln -s ${ZPREZTO}/zlogout ~/.zlogout
+    ln -s ${ZPREZTO}/zprezto ~/.zprezto
+    ln -s ${ZPREZTO}/zpreztorc ~/.zpreztorc
+    ln -s ${ZPREZTO}/zprofile ~/.zprofile
+    ln -s ${ZPREZTO}/zshenv ~/.zshenv
+    ln -s ${ZPREZTO}/zshrc ~/.zshrc
+}
+
+update_vim_submodules () {
+    echo "Updating vim submodules"
+    cd ${DOTFILES}
+    mkdir -p ~/.vim/tmp/undo
+    git submodule init
+    git submodule update
+    git submodule foreach git pull origin master --recurse-submodules
+}
+
+update_zprezto_submoules () {
+    echo "Updating zprezto submodules"
+    cd ${ZPREZTO}
+    git submodule init
+    git submodule update
+    git submodule foreach git pull origin master --recurse-submodules
+}
+
+if [ ${REMOVE_EXISTING} ]; then
+    checkout_repos
+    remove_dotfiles
+    create_symlinks
+else
+    update_repos
+    update_vim_submodules
+    update_zprezto_submoules
 fi
-
-echo "Creating dotfile symlinks"
-ln -s ~/Code/dotfiles/ackrc ~/.ackrc
-ln -s ~/Code/dotfiles/dotcss ~/.css
-ln -s ~/Code/dotfiles/dotjs ~/.js
-ln -s ~/Code/dotfiles/gvimrc ~/.gvimrc
-ln -s ~/Code/dotfiles/inputrc ~/.inputrc
-ln -s ~/Code/dotfiles/rvmrc ~/.rvmrc
-ln -s ~/Code/dotfiles/ssh_config ~/.ssh/config
-ln -s ~/Code/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/Code/dotfiles/vim ~/.vim
-ln -s ~/Code/dotfiles/vim_colors ~/.vim_colors
-ln -s ~/Code/dotfiles/vimrc ~/.vimrc
-ln -s ~/Code/oh-my-zsh/zshrc ~/.zshrc
-ln -s ~/Code/oh-my-zsh ~/.oh-my-zsh
-
-echo "Updating vim submodules"
-cd ~/Code/dotfiles
-mkdir -p ~/.vim/tmp/undo
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "Removing non-functioning bundle checkouts"
-#TODO work out why I have to do this
-rm -rf ~/Code/dotfiles/vim/bundle/vim-powerline/
-rm -rf ~/Code/dotfiles/vim/bundle/Zenburn/
-rm -rf ~/Code/dotfiles/vim/bundle/dbext.vim/
-rm -rf ~/Code/dotfiles/vim/bundle/TWiki-Syntax/
-rm -rf ~/Code/dotfiles/vim/bundle/tabular/
-rm -rf ~/Code/dotfiles/vim/bundle/vim-makegreen/
-rm -rf ~/Code/dotfiles/vim/bundle/vim-session/
-vim +BundleInstall
-
-echo "Updating zsh submodules"
-cd ~/Code/oh-my-zsh
-git submodule init
-git submodule update
-git submodule foreach git pull origin master --recurse-submodules
