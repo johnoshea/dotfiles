f3896dab75d66c70af346c87894407762d0709e4
John O'Shea <john.m.oshea@gmail.com>
2013-02-17 12:07:06 +0000

    Fixed bad assumption in install.sh

--- install.sh
+++ install.sh
@@ -23,56 +23,31 @@ update_repos () {
 checkout_repos () {
     echo "Checking out repos"
     rm -rf ${ZPREZTO} && clone https://github.com/johnoshea/zprezto.git ${ZPREZTO}
-    rm -rf ${DOTFILES} && clone https://github.com/johnoshea/dotfiles ${DOTFILES}
-}
-
-remove_dotfiles () {
-    echo "Removing existing home dotfiles"
-    rm -f ~/.ackrc
-    rm -f ~/.css
-    rm -f ~/.js
-    rm -f ~/.gvimrc
-    rm -f ~/.inputrc
-    rm -f ~/.rvmrc
-    rm -f ~/.ssh/config
-    rm -f ~/.terminfo
-    rm -f ~/.tmux.conf
-    rm -f ~/.terminfo
-    rm -f ~/.vim
-    rm -f ~/.vim_colors
-    rm -f ~/.vimrc
-    rm -f ~/.zlogin
-    rm -f ~/.zlogout
-    rm -f ~/.zprezto
-    rm -f ~/.zpreztorc
-    rm -f ~/.zprofile
-    rm -f ~/.zshenv
-    rm -f ~/.zshrc
 }
 
 
-create_symlinks () {
+make_symlinks () {
     echo "Creating dotfile symlinks"
-    ln -s ${DOTFILES}/ackrc ~/.ackrc
-    ln -s ${DOTFILES}/dotcss ~/.css
-    ln -s ${DOTFILES}/dotjs ~/.js
-    ln -s ${DOTFILES}/gvimrc ~/.gvimrc
-    ln -s ${DOTFILES}/inputrc ~/.inputrc
-    ln -s ${DOTFILES}/rvmrc ~/.rvmrc
-    ln -s ${DOTFILES}/ssh_config ~/.ssh/config
-    ln -s ${DOTFILES}/tmux.conf ~/.tmux.conf
-    ln -s ${DOTFILES}/terminfo ~/.terminfo
-    ln -s ${DOTFILES}/vim ~/.vim
-    ln -s ${DOTFILES}/vim_colors ~/.vim_colors
-    ln -s ${DOTFILES}/vimrc ~/.vimrc
+    ln -sf ${DOTFILES}/ackrc ~/.ackrc
+    ln -sf ${DOTFILES}/dotcss ~/.css
+    ln -sf ${DOTFILES}/dotjs ~/.js
+    ln -sf ${DOTFILES}/gvimrc ~/.gvimrc
+    ln -sf ${DOTFILES}/inputrc ~/.inputrc
+    ln -sf ${DOTFILES}/rvmrc ~/.rvmrc
+    ln -sf ${DOTFILES}/ssh_config ~/.ssh/config
+    ln -sf ${DOTFILES}/tmux.conf ~/.tmux.conf
+    ln -sf ${DOTFILES}/terminfo ~/.terminfo
+    ln -sf ${DOTFILES}/vim ~/.vim
+    ln -sf ${DOTFILES}/vim_colors ~/.vim_colors
+    ln -sf ${DOTFILES}/vimrc ~/.vimrc
 
-    ln -s ${ZPREZTO}/zlogin ~/.zlogin
-    ln -s ${ZPREZTO}/zlogout ~/.zlogout
-    ln -s ${ZPREZTO}/zprezto ~/.zprezto
-    ln -s ${ZPREZTO}/zpreztorc ~/.zpreztorc
-    ln -s ${ZPREZTO}/zprofile ~/.zprofile
-    ln -s ${ZPREZTO}/zshenv ~/.zshenv
-    ln -s ${ZPREZTO}/zshrc ~/.zshrc
+    ln -sf ${ZPREZTO}/zlogin ~/.zlogin
+    ln -sf ${ZPREZTO}/zlogout ~/.zlogout
+    ln -sf ${ZPREZTO}/zprezto ~/.zprezto
+    ln -sf ${ZPREZTO}/zpreztorc ~/.zpreztorc
+    ln -sf ${ZPREZTO}/zprofile ~/.zprofile
+    ln -sf ${ZPREZTO}/zshenv ~/.zshenv
+    ln -sf ${ZPREZTO}/zshrc ~/.zshrc
 }
 
 update_vim_submodules () {
@@ -92,10 +67,9 @@ update_zprezto_submoules () {
     git submodule foreach git pull origin master --recurse-submodules
 }
 
+make_symlinks
 if [ ${REMOVE_EXISTING} ]; then
     checkout_repos
-    remove_dotfiles
-    create_symlinks
 else
     update_repos
     update_vim_submodules
