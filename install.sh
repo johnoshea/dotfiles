565711ff0a7cc6d70aac533a2ae321e38990b929
John O'Shea <john.m.oshea@gmail.com>
2015-06-02 12:23:45 +0100

    Start using Pylint

--- install.sh
+++ install.sh
@@ -73,6 +73,7 @@ make_symlinks () {
     ln -sf ~/${DOTFILES}/vimrc ~/.vimrc
     ln -sf ~/${DOTFILES}/vimperator ~/.vimperator
     ln -sf ~/${DOTFILES}/vimperatorrc ~/.vimperatorrc
+    ln -sf ~/${DOTFILES}/pylintrc ~/.pylintrc
 
     ln -sf ~/${ZPREZTO}/runcoms/zlogin ~/.zlogin
     ln -sf ~/${ZPREZTO}/runcoms/zlogout ~/.zlogout
