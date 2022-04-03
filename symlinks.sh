dd8ae23dbbfdd24a3f80246ebf402e2646422f97
John O'Shea <john.m.oshea@gmail.com>
2022-04-03 11:47:08 -0600

    Add basic 'create symlinks' script
    This should be replaced by something like `stow` but was useful enough at the time

---
+++ symlinks.sh
@@ -0,0 +1,37 @@
+ln -sf ~/src/dotfiles/CFUserTextEncoding ~/.CFUserTextEncoding
+ln -sf ~/src/dotfiles/curlrc ~/.curlrc
+ln -sf ~/src/dotfiles/digrc ~/.digrc
+ln -sf ~/src/dotfiles/editorconfig ~/.editorconfig
+ln -sf ~/src/dotfiles/eslintrc.js ~/.eslintrc.js
+ln -sf ~/src/dotfiles/gemrc ~/.gemrc
+ln -sf ~/src/dotfiles/gitattributes ~/.gitattributes
+ln -sf ~/src/dotfiles/gitconfig.Darwin ~/.gitconfig
+ln -sf ~/src/dotfiles/gitignore_global ~/.gitignore_global
+ln -sf ~/src/dotfiles/hgignore_global ~/.hgignore_global
+ln -sf ~/src/dotfiles/inputrc ~/.inputrc
+ln -sf ~/src/dotfiles/mdlrc ~/.mdlrc
+ln -sf ~/src/dotfiles/pdbrc.py ~/.pdbrc.py
+ln -sf ~/src/dotfiles/psqlrc ~/.psqlrc
+ln -sf ~/src/dotfiles/pylintrc ~/.pylintrc
+ln -sf ~/src/dotfiles/terminfo ~/.terminfo
+ln -sf ~/src/dotfiles/tigrc ~/.tigrc
+ln -sf ~/src/dotfiles/tmux-osx.conf ~/.tmux-osx.conf
+ln -sf ~/src/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/src/dotfiles/vim ~/.vim
+ln -sf ~/src/dotfiles/vimrc ~/.vimrc
+ln -sf ~/src/dotfiles/wgetrc ~/.wgetrc
+ln -sf ~/src/prezto/runcoms/zlogin ~/.zlogin
+ln -sf ~/src/prezto/runcoms/zlogout ~/.zlogout
+ln -sf ~/src/prezto ~/.zprezto
+ln -sf ~/src/prezto/runcoms/zpreztorc ~/.zpreztorc
+ln -sf ~/src/prezto/runcoms/zprofile ~/.zprofile
+ln -sf ~/src/prezto/runcoms/zshenv ~/.zshenv
+ln -sf ~/src/prezto/runcoms/zshrc ~/.zshrc
+ln -sf ~/src/prezto/runcoms/zshrc.local ~/.zshrc.local
+ln -sf ~/Documents/1\ In/ ~/1\ In
+ln -sf ~/Documents/2\ Out/ ~/2\ Out
+ln -sf ~/Documents/3\ Current/ ~/3\ Current
+ln -sf ~/Documents/4\ Archive ~/4\ Archive
+ln -sf ~/Documents/AWS/Certs/ ~/aws_certs
+ln -sf ~/Documents/bin/ ~/bin
+ln -sf ~/Library/Mobile\ Documents/com~/apple~CloudDocs ~/iCloud
