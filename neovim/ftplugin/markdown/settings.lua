3e55e9061fc05b08740e94839965a1e3aabd6210
John O'Shea <john.m.oshea@gmail.com>
2022-11-25 21:11:29 -0700

    Fix Marked 2 keymap

--- neovim/ftplugin/markdown/settings.lua
+++ neovim/ftplugin/markdown/settings.lua
@@ -2,4 +2,4 @@ local keymap = vim.keymap.set       -- shorten function name
 local opts = { silent = true }      -- silent keymap option
 
 -- Use 'Marked.app' for Markdown previews
-keymap("n", "<leader>ma", ':!open -a /Applications/Marked 2.app "%:p"<cr>', opts)
+keymap("n", "<leader>ma", ':!open -a "/Applications/Marked 2.app" "%:p"<cr>', opts)
