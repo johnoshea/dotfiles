4bb5d2f7db2b044ab16643d753dc0cabcd225465
John O'Shea <john.m.oshea@gmail.com>
2023-02-04 16:06:59 -0700

    Add neovim 'after' configuration for Python files

--- neovim/after/ftplugin/python.lua
+++ neovim/after/ftplugin/python.lua
@@ -1,3 +1,11 @@
-vim.opt_local.foldmethod = "indent" -- Use indent level as fold markers
-vim.opt_local.foldnestmax = 2 -- Only fold up to method level
-vim.opt_local.foldignore = '"""' -- Don't  fold docstrings
+vim.opt_local.expandtab = true
+vim.opt_local.autoindent = true
+vim.opt_local.smarttab = true
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+-- vim.opt_local.fileformat = "unix"
+vim.opt_local.textwidth = 80
+
+-- Set the indent after opening parenthesis
+vim.g.pyindent_open_paren = vim.bo.shiftwidth
