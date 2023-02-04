8cb39bf4ba40b54420d717c17a2944b3c3129513
John O'Shea <john.m.oshea@gmail.com>
2023-02-04 16:32:11 -0700

    Whitespace-only change

--- neovim/lua/jos/nvim-ufo.lua
+++ neovim/lua/jos/nvim-ufo.lua
@@ -3,6 +3,6 @@
 -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
 require('ufo').setup({
     provider_selector = function(bufnr, filetype, buftype)
-        return {'treesitter', 'indent'}
+      return {'treesitter', 'indent'}
     end
 })
