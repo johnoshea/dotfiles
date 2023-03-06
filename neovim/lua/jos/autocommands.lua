63f8d81103a2165bff09c08ba854159e78cb00c8
John O'Shea <john.m.oshea@gmail.com>
2023-03-06 12:32:09 -0700

    Remove format-on-save from autocommands.lua

--- neovim/lua/jos/autocommands.lua
+++ neovim/lua/jos/autocommands.lua
@@ -68,20 +68,3 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 		end
 	end,
 })
-
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-require("null-ls").setup({
-    -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format( { bufnr = bufnr } )
-                end,
-            })
-        end
-    end,
-})
