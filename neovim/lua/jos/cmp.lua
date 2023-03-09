0e3bff2c8f6a64574478b51e5696025b1b856877
John O'Shea <john.m.oshea@gmail.com>
2023-03-09 10:26:41 -0700

    Use c-<space> for cmp complete

--- neovim/lua/jos/cmp.lua
+++ neovim/lua/jos/cmp.lua
@@ -54,7 +54,7 @@ cmp.setup({
 		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-		["<C-o>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<C-space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
