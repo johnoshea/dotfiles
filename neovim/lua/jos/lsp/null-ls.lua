79c647d7d891cdaa5606f5b07c2d483d765ea21b
John O'Shea <john.m.oshea@gmail.com>
2023-03-09 10:27:52 -0700

    Add null-ls diagnostics
    for linters that Mason doesn't provide

--- neovim/lua/jos/lsp/null-ls.lua
+++ neovim/lua/jos/lsp/null-ls.lua
@@ -4,43 +4,59 @@ if not null_ls_status_ok then
 end
 
 local formatting = null_ls.builtins.formatting
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		-- diagnostics should be configured using Mason wherever possible
-		formatting.trim_whitespace,
-		formatting.trim_newlines,
+  debug = false,
+  sources = {
+    -- diagnostics should be configured using Mason wherever possible
+    formatting.trim_whitespace,
+    formatting.trim_newlines,
 
-		-- Python
-		formatting.black,
-		formatting.isort,
+    -- Python
+    diagnostics.ruff,
+    diagnostics.vulture,
+    formatting.black,
+    formatting.isort,
+    formatting.djlint,
 
-		-- Lua
-		formatting.stylua,
+    -- Lua
+    formatting.stylua,
 
-		-- HCL
-		formatting.terraform_fmt,
+    -- HCL
+    formatting.terraform_fmt,
 
-		-- generic
-		formatting.prettier.with({
-			extra_filetypes = { "toml", "md" },
-			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-		}),
-	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
+    -- yaml
+    diagnostics.yamllint,
+    formatting.yamlfmt,
+
+    -- xml
+    -- formatting.xmlformatter,
+
+    -- bash/sh
+    diagnostics.shellcheck,
+
+    -- git
+    diagnostics.commitlint,
+
+    -- generic
+    formatting.prettier.with({
+      extra_filetypes = { "toml", "md" },
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    }),
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
