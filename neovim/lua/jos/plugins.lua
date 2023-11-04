0d4f4ef1fdc79a0da753ebbd82548546a1692568
John O'Shea <john.m.oshea@gmail.com>
2023-11-04 14:43:31 -0600

    Add octo.nvim plugin

--- neovim/lua/jos/plugins.lua
+++ neovim/lua/jos/plugins.lua
@@ -247,6 +247,19 @@ return packer.startup(function(use)
     end,
   })
   use({ "rhysd/committia.vim" })
+  use({
+    "pwntester/octo.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup({
+        ssh_aliases = { ["wordbank.github.com"] = "github.com" },
+      })
+    end,
+  })
 
   -- Tmux
   use({ "christoomey/vim-tmux-navigator" })
