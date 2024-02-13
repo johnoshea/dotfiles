3f8ef95683670f679347d969c1aaef28365c05b9
John O'Shea <john.m.oshea@gmail.com>
2024-02-13 09:33:16 -0700

    fix: switch treesitter to auto-install parsers

--- neovim/lua/jos/treesitter.lua
+++ neovim/lua/jos/treesitter.lua
@@ -9,31 +9,7 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = {
-    "bash",
-    "css",
-    "diff",
-    "dockerfile",
-    "gitcommit",
-    "gitignore",
-    "go",
-    "hcl",
-    "html",
-    "htmldjango",
-    "javascript",
-    "json",
-    "lua",
-    "make",
-    "markdown",
-    "markdown_inline",
-    "python",
-    "regex",
-    "sql",
-    "terraform",
-    "toml",
-    "vim",
-    "yaml",
-  }, -- one of "all" or a list of languages
+  auto_install = true,
   ignore_install = { "" }, -- List of parsers to ignore installing
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   highlight = {
