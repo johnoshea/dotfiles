652a0e443a5562ae1cca75bb6c78c1875ebe20bf
John O'Shea <github@johnoshea.org>
2024-12-07 09:58:54 -0700

    Restrict nvim-treesitter-context to 10 lines of context

--- dot_config/nvim/lua/core/plugins/nvim-treesitter-context.lua
+++ dot_config/nvim/lua/core/plugins/nvim-treesitter-context.lua
@@ -1,3 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
+  config = function()
+    require("treesitter-context").setup({
+      max_lines = 10, -- How many lines the window should span. Values <= 0 mean no limit.
+    })
+  end,
 }
