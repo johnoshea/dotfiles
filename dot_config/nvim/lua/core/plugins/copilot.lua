6cfdf7bd3846874a814c72d5ff8e79b63435856c
John O'Shea <github@johnoshea.org>
2026-04-05 15:17:29 -0600

    Remove copilot-lsp from neovim config

--- dot_config/nvim/lua/core/plugins/copilot.lua
+++ dot_config/nvim/lua/core/plugins/copilot.lua
@@ -1,8 +1,5 @@
 return {
   "zbirenbaum/copilot.lua",
-  requires = {
-    "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
-  },
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
