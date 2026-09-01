38780b00addb5119e53e39c4a204e8989e133cf1
John O'Shea <github@johnoshea.org>
2026-09-01 17:47:40 -0600

    Default grug-far to use ripgrep instead of astgrep

--- dot_config/nvim/lua/core/plugins/grug-far.lua
+++ dot_config/nvim/lua/core/plugins/grug-far.lua
@@ -1,12 +1,8 @@
 return {
   "MagicDuck/grug-far.nvim",
   config = function()
-    require("grug-far").setup({
-      -- options, see Configuration section below
-      -- there are no required options atm
-      -- engine = 'ripgrep' is default, but 'astgrep' can be specified
-      engine = "astgrep",
-    })
+    -- ripgrep is the default engine; `<localleader>e` swaps to ast-grep in the buffer.
+    require("grug-far").setup({})
     vim.keymap.set("n", "<leader>gf", "<cmd>GrugFar<CR>", { desc = "Open [g]rug-[f]ar" })
   end,
 }
