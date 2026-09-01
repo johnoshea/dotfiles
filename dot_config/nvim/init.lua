18b5323879cb9e30b03f8377680b1d967b1b09ca
John O'Shea <github@johnoshea.org>
2026-09-01 17:38:44 -0600

    Stick to rose-pine as my nvim colorscheme

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -39,7 +39,7 @@ require("lazy").setup({
       },
     },
   },
-  install = { colorscheme = { "catppuccin" } }, -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "rose-pine" } }, -- colorscheme that will be used when installing plugins.
   checker = { enabled = true, notify = false }, -- automatically check for plugin updates
   change_detection = {
     notify = false,
